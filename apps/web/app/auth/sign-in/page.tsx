4fdfd6c96daf28bae075419f69be0a0ecc30e45e
Altay <altay@zebrastik.com>
2023-10-28 12:33:07 +0300

    feat(web): use server-actions for supabase login

--- apps/web/app/auth/sign-in/page.tsx
+++ apps/web/app/auth/sign-in/page.tsx
@@ -1,31 +1,40 @@
-"use client";
-
-import { createSupabaseBrowserClient } from "@/lib/supabase/browser";
-import { Button, Title } from "@tremor/react";
+import { Title, Button, Text } from "@tremor/react";
+import { cookies } from "next/headers";
+import { createSupabaseServerClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function Page() {
-  const supabase = createSupabaseBrowserClient();
-  const callbackURL = new URL("/auth/callback", window.location.href);
+  async function handleSignIn() {
+    "use server";
+
+    const supabase = createSupabaseServerClient(cookies());
+
+    const result = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: new URL(
+          "/auth/callback",
+          process.env.NEXT_PUBLIC_VERCEL_URL
+        ).toString(),
+      },
+    });
+
+    redirect(result.data.url!);
+  }
 
   return (
     <div className="flex justify-center flex-col max-w-xs mx-auto">
       <Title>@browsercare/web</Title>
 
-      <Button
-        icon={GitHubLogoIcon}
-        className="mt-4"
-        onClick={async () => {
-          await supabase.auth.signInWithOAuth({
-            provider: "github",
-            options: {
-              redirectTo: callbackURL.toString(),
-            },
-          });
-        }}
-      >
-        Sign in with GitHub
-      </Button>
+      <form action={handleSignIn} className="mt-4">
+        <Button type="submit" className="w-full">
+          <span className="flex items-center justify-center flex-row gap-2">
+            <GitHubLogoIcon />
+            <span>Sign in with GitHub</span>
+          </span>
+        </Button>
+      </form>
     </div>
   );
 }

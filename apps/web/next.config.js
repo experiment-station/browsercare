fd790dda611811b21d4e19eaebe8359aaf534e48
Altay <altay@zebrastik.com>
2023-11-06 13:58:51 +0300

    chore: lint

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -17,6 +16,7 @@ const nextConfig = {
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
+        source: "/api/:path*",
       },
     ];
   },

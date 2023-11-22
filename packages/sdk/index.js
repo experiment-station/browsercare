f5f19aad1211e8f505160746cd1ed95ffad20d69
Altay <altay@zebrastik.com>
2023-11-22 18:16:11 +0300

    refactor(sdk): make the JS code as old as possible

--- packages/sdk/index.js
+++ packages/sdk/index.js
@@ -2,15 +2,25 @@
   try {
     var scripts = document.getElementsByTagName("script");
     var currentScript = scripts[scripts.length - 1];
-    var scriptUrl = new URL(currentScript.src);
+    var scriptSrc = currentScript.src;
+
+    var getQueryStringValue = function (key, url) {
+      var query = url.substring(url.indexOf("?") + 1);
+      var vars = query.split("&");
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (decodeURIComponent(pair[0]) === key) {
+          return decodeURIComponent(pair[1]);
+        }
+      }
+      return null;
+    };
 
     var makeAPICall = function (url, callback) {
       var httpRequest = new XMLHttpRequest();
-
       if (!httpRequest) {
         throw new Error("Failed to create XMLHttpRequest instance");
       }
-
       httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
           if (httpRequest.status === 200) {
@@ -20,13 +30,12 @@
           }
         }
       };
-
       httpRequest.open("GET", url);
       httpRequest.send();
     };
 
-    var project = scriptUrl.searchParams.get("project");
-    var env = scriptUrl.searchParams.get("env") || "production";
+    var project = getQueryStringValue("project", scriptSrc);
+    var env = getQueryStringValue("env", scriptSrc) || "production";
 
     var API_URL =
       env === "production"
@@ -40,7 +49,6 @@
 
         if (project === "browsercare/demo") {
           var node = document.createElement("pre");
-
           node.innerHTML = JSON.stringify(
             {
               project: project,
@@ -50,7 +58,6 @@
             null,
             2
           );
-
           var body = document.getElementsByTagName("body")[0];
           body.appendChild(node);
         }
